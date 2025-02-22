5e0441b6b8c7c56b7044d8e01d7700dbb6b95ac1
Eimer Castro <ewcastroh10@gmail.com>
2025-02-22 11:52:01 -0500

    Initial commit.

---
+++ src/main/java/com/ewch/testing/springboot/app/SpringbootTestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ewch.testing.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestingAppApplication.class, args);
+	}
+
+}
