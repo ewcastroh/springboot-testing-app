4dc090bc9aefc7c6f769845292c7e3bf7071c7da
Eimer Castro <ewcastroh10@gmail.com>
2025-02-23 16:38:33 -0500

    Implementing native query using Query annotation into AccountRepository.

--- src/main/java/com/ewch/testing/springboot/app/repositories/AccountRepository.java
+++ src/main/java/com/ewch/testing/springboot/app/repositories/AccountRepository.java
@@ -2,7 +2,10 @@ package com.ewch.testing.springboot.app.repositories;
 
 import com.ewch.testing.springboot.app.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -10,4 +13,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // List<Account> findAll();
     // Account findById(Long id);
     // void update(Account account);
+
+    @Query("SELECT a FROM Account a WHERE a.person = ?1")
+    Optional<Account> findByPerson(String person);
 }
